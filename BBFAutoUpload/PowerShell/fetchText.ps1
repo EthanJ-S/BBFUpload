9dbfb97e0245454f3a284c99e72f9ebf473f7420
EthanJ-S <78390216+EthanJ-S@users.noreply.github.com>
2022-08-24 21:09:28 -0400

    Add files via upload

---
+++ BBFAutoUpload/PowerShell/fetchText.ps1
@@ -0,0 +1,15 @@
+﻿import-module 'C:\BBFAutoUpload\PowerShell\textFilter.psm1';
+
+import-module 'C:\BBFAutoUpload\PowerShell\deleteText.psm1';
+
+$filePath = 'C:\BBFAutoUpload\tempFiles\sermonText';
+
+$outPath = 'C:\BBFAutoUpload\videoDetails\';
+
+$title = GetStringBetweenTwoStrings -firstString "title: '" -secondString "'," -importPath $filePath;
+$title = $title.replace('&quot;','"') | out-file "C:\BBFAutoUpload\videoInformation\title.txt";
+   
+
+
+$description = GetStringBetweenTwoStrings -firstString '<span class="markdown stylized-lists"><p>' -secondString '</p><p><a href="https://bbfohio.com"' -importPath $filePath | 
+    out-file "C:\BBFAutoUpload\videoInformation\description.txt";
---
+++ BBFAutoUpload/PowerShell/textFilter.psm1
@@ -0,0 +1,15 @@
+﻿function GetStringBetweenTwoStrings($firstString, $secondString, $importPath){
+
+    #Get content from file
+    $file = Get-Content $importPath
+
+    #Regex pattern to compare two strings
+    $pattern = "$firstString(.*?)$secondString"
+
+    #Perform the opperation
+    $result = [regex]::Match($file,$pattern).Groups[1].Value
+
+    #Return result
+    return $result
+
+}
